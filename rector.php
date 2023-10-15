cdced77ee51223a10e4c20550b04e857996529d0
Dmitriy Krivopalov <dimarik82@gmail.com>
2023-10-15 16:15:28 +0300

    fix workflows

--- rector.php
+++ rector.php
@@ -20,7 +20,7 @@ return static function (RectorConfig $rectorConfig): void {
 
     // define sets of rules
     $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_81,
         SetList::PHP_81,
+        SetList::PHP_82,
     ]);
 };
