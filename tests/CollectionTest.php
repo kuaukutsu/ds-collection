68e9ce31abde08bdf3edcbdfff44e39ba111d81f
Dmitriy Krivopalov <dimarik82@gmail.com>
2025-08-07 13:55:25 +0300

    refactoring clear and copy remove (#24)

--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -9,10 +9,11 @@ use PHPUnit\Framework\TestCase;
 use kuaukutsu\ds\collection\CollectionTypeException;
 use kuaukutsu\ds\collection\tests\stub\Dto;
 use kuaukutsu\ds\collection\tests\stub\DtoCollection;
+use kuaukutsu\ds\collection\tests\stub\StdCollection;
 
 final class CollectionTest extends TestCase
 {
-    public function testCollectionConstruct(): void
+    public function testConstruct(): void
     {
         $collection = new DtoCollection(
             new Dto(1, 'first'),
@@ -23,35 +24,25 @@ final class CollectionTest extends TestCase
         self::assertCount(3, $collection);
     }
 
-    public function testCollectionAdd(): void
+    public function testAddAndRemove(): void
     {
+        $item = new Dto(3, 'third');
+
         $collection = new DtoCollection();
         $collection->attach(new Dto(1, 'first'));
         $collection->attach(new Dto(2, 'second'));
+        $collection->attach($item);
 
+        self::assertCount(3, $collection);
+
+        $collection->detach($item);
         self::assertCount(2, $collection);
 
         $collection->clear();
-
         self::assertCount(0, $collection);
     }
 
-    public function testCollectionRemove(): void
-    {
-        $item = new Dto(2, 'second');
-
-        $collection = new DtoCollection();
-        $collection->attach(new Dto(1, 'first'));
-        $collection->attach($item);
-
-        self::assertCount(2, $collection);
-
-        $collection->detach($item);
-
-        self::assertCount(1, $collection);
-    }
-
-    public function testCollectionIterator(): void
+    public function testIterator(): void
     {
         $collection = new DtoCollection();
         $collection->attach(new Dto(1, 'first'));
@@ -64,31 +55,23 @@ final class CollectionTest extends TestCase
         }
     }
 
-    public function testCollectionMerge(): void
+    public function testMerge(): void
     {
         $collection = new DtoCollection();
         $collection->attach(new Dto(1, 'first'));
         $collection->attach(new Dto(2, 'second'));
 
         $collectionSecond = new DtoCollection(new Dto(3, 'merge'));
-
         $collection->merge($collectionSecond);
-
         self::assertCount(3, $collection);
 
-        /**
-         * @var Dto $c1
-         * @var Dto $c2
-         * @var Dto $c3
-         */
         [$c1, $c2, $c3] = $collection->toArray();
-
         self::assertEquals(1, $c1->id);
         self::assertEquals(2, $c2->id);
         self::assertEquals(3, $c3->id);
     }
 
-    public function testCollectionFilter(): void
+    public function testFilter(): void
     {
         $collection = new DtoCollection();
         $collection->attach(new Dto(1, 'test'));
@@ -96,20 +79,14 @@ final class CollectionTest extends TestCase
         $collection->attach(new Dto(3, 'test'));
 
         $collectionNew = $collection->filter(static fn(Dto $dto): bool => $dto->name === 'test');
-
         self::assertCount(2, $collectionNew);
 
-        /**
-         * @var Dto $c1
-         * @var Dto $c2
-         */
         [$c1, $c2] = $collectionNew->toArray();
-
         self::assertEquals(1, $c1->id);
         self::assertEquals(3, $c2->id);
     }
 
-    public function testCollectionOperations(): void
+    public function testContains(): void
     {
         $object = new Dto(3, 'third');
 
@@ -117,35 +94,10 @@ final class CollectionTest extends TestCase
         $collection->attach(new Dto(1, 'first'));
         $collection->attach(new Dto(2, 'second'));
 
-        self::assertCount(2, $collection);
-
         self::assertFalse($collection->contains($object));
 
         $collection->attach($object);
-
         self::assertTrue($collection->contains($object));
-
-        self::assertCount(3, $collection);
-
-        $collection->detach($object);
-
-        self::assertCount(2, $collection);
-
-        $collection->clear();
-
-        self::assertCount(0, $collection);
-    }
-
-    public function testCollectionArray(): void
-    {
-        $array = [
-            new Dto(1, 'first'),
-            new Dto(2, 'second'),
-        ];
-
-        $collection = new DtoCollection(...$array);
-
-        self::assertEquals($array, $collection->toArray());
     }
 
     public function testTypeExceptionCreateConstruct(): void
@@ -159,7 +111,7 @@ final class CollectionTest extends TestCase
         new DtoCollection(new stdClass());
     }
 
-    public function testTypeExceptionCreateAdd(): void
+    public function testTypeExceptionAttach(): void
     {
         $collection = new DtoCollection();
 
@@ -170,6 +122,20 @@ final class CollectionTest extends TestCase
          * @phpstan-ignore argument.type
          */
         $collection->attach(new stdClass());
+    }
+
+    public function testTypeExceptionMerge(): void
+    {
+        $collection = new DtoCollection();
+        $collectionMerge = new StdCollection(new stdClass());
+
+        $this->expectException(CollectionTypeException::class);
+
+        /**
+         * @psalm-suppress InvalidArgument exception
+         * @phpstan-ignore argument.type
+         */
+        $collection->merge($collectionMerge);
     }
 
     public function testDoesNotLeakMemory(): void
